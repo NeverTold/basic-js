fa48f602059c7ecb88a3a9eb1fc046f4e7d55672
NeverTold <149240019+NeverTold@users.noreply.github.com>
2025-01-03 03:02:05 +0700

    fix: carbon dating data types fix

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,10 +18,13 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-  if (typeof Number(sampleActivity) === 'NaN') {
-    return false;
-  } else {
+  if (sampleActivity && typeof Number(sampleActivity) === 'number' && typeof sampleActivity === 'string' && Number(sampleActivity) < 15 && Number(sampleActivity) > 0) {
+    console.log(typeof sampleActivity);
+    console.log(Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD)));
     return Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
+  }
+   else {
+    return false;
   }
   // remove line with error and write your code here
 }
