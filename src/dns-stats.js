6d7c86fc1699815dd90ccd95373da1d68fca07a6
NeverTold <149240019+NeverTold@users.noreply.github.com>
2025-01-07 06:59:19 +0700

    feature: dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,11 +22,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let arr = [];
+  let maxLen = 0;
+  let result = {};
+  for (let i = 0; i < domains.length; i++) {
+    let tempArr = domains[i].split('.');
+    if (tempArr.length > maxLen) {
+      maxLen = tempArr.length;
+    }
+    arr.push(tempArr.reverse().map(value => '.' + value));
+  }
+  for (let i = 0; i < arr.length; i++){
+    let str = '';
+    console.log(str)
+    for(let j = 0; j < arr[i].length; j++){
+      if(result[str += arr[i][j]] === undefined){
+        result[str] = 1;
+      } else {
+        result[str]++;
+      }
+    }
+  }
+  return result;
 }
-
 module.exports = {
   getDNSStats
 };
