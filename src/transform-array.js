fcb87561a7032c0a35ff0e86974f465c40e1c778
NeverTold <149240019+NeverTold@users.noreply.github.com>
2025-01-07 02:21:33 +0700

    feature: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!(arr instanceof Array)) {
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }
+  const tempArr = arr.map((element) => element);
+  let result = [];
+  let j = 0;
+  for (let i = 0; i < tempArr.length; i += 1) {
+    if (tempArr[i] === '--discard-next') {
+      tempArr[i] = 'del'; 
+      result.push(tempArr[i]);
+      tempArr[i+1] = 'del';
+    } if (tempArr[i] === '--discard-prev') {
+      tempArr[i] = 'del';
+      if (!(tempArr[i - 1] === 'del') && result){
+        result.pop();
+      result.pop();
+      }
+    } if (tempArr[i] === '--double-next') {
+      tempArr[i] = 'del';
+      if (!(tempArr[i + 1] === 'del') && tempArr[i + 1]) {
+      tempArr[i] = tempArr[i + 1];
+      result.push(tempArr[i]);
+      }
+    } if (tempArr[i] === '--double-prev') {
+      tempArr[i] = 'del';
+      if (!tempArr[i - 1] === 'del') {
+      tempArr[i] = tempArr[i - 1];
+      result.push(tempArr[i]);
+      }
+    } else {
+      result.push(tempArr[i]);
+ }
+  }
+  return result.filter((value) => (value !== 'del'));
 }
 
 module.exports = {
