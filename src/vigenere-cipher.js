239d398083c3ee187f1a36f09d79e3099fceb27a
NeverTold <149240019+NeverTold@users.noreply.github.com>
2025-01-07 05:41:18 +0700

    feature: vingere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,16 +19,59 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    constructor(isDirect = true) {
+      this.isDirect = isDirect;
+      this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+  
+    _process(text, key, encrypting) {
+  
+      let result = '';
+      let keyIndex = 0;
+  
+      for (let i = 0; i < text.length; i++) {
+        const char = text[i].toUpperCase();
+        const charIndex = this.alphabet.indexOf(char);
+  
+        if (charIndex === -1) {
+          result += text[i];
+          continue;
+        }
+  
+        const keyChar = key[keyIndex % key.length].toUpperCase();
+        const keyCharIndex = this.alphabet.indexOf(keyChar);
+  
+        let processedIndex;
+        if (encrypting) {
+          processedIndex = (charIndex + keyCharIndex) % this.alphabet.length;
+        } else {
+            processedIndex = (charIndex - keyCharIndex + this.alphabet.length) % this.alphabet.length;
+  
+        }
+        result += this.alphabet[processedIndex];
+        keyIndex++;
+      }
+          return this.isDirect ? result : result.split("").reverse().join("");
+    }
+  
+  
+    encrypt(text, key) {
+      if (!text || !key) {
+        throw new Error('Incorrect arguments!');
+      }
+      return this._process(text, key, true);
+    }
+  
+    decrypt(text, key) {
+      if (!text || !key) {
+        throw new Error('Incorrect arguments!');
+      }
+        return this._process(text, key, false);
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
+
 
 module.exports = {
   VigenereCipheringMachine
